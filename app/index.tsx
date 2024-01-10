a62af14afdf70b9195579ed562e849ceb3c68741
Freddy Febres <freddyrfa@gmail.com>
2024-01-09 22:17:01 -0400

    ✨ Adding initial fetch for star wars movies

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,34 @@
+import { ActivityIndicator, Button, Text } from 'react-native';
+import { FlashList } from '@shopify/flash-list';
 import { Link } from 'expo-router';
-import React from 'react'
-import { Button, Text, View } from "react-native";
+import { useQuery } from '@tanstack/react-query';
+
+import { ApiResponse, apiFetch } from '../services/api';
+import { Films } from '../types/films';
 
 export default function Page() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Page</Text>
-      <Link href={'detail'} asChild>
-        <Button title='Go to Detail Screen' />
-      </Link>
-    </View>
-  )
+   const { data, isLoading } = useQuery<ApiResponse<Films>>({
+      queryFn: () => apiFetch('GET', 'films/'),
+      queryKey: ['films'],
+      refetchOnMount: false,
+      refetchOnWindowFocus: false
+   });
+
+   if (isLoading) return <ActivityIndicator />;
+
+   return (
+      <>
+         <Text>Page</Text>
+         <Link href={'detail'} asChild>
+            <Button title="Go to Detail Screen" />
+         </Link>
+         <FlashList
+            data={data!.results}
+            estimatedItemSize={200}
+            renderItem={({ item }) => (
+               <Text style={{ fontSize: 15, color: 'black' }}>{item.title}</Text>
+            )}
+         />
+      </>
+   );
 }
