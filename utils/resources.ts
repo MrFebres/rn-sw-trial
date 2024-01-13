73d181fad18f8f33361b9e186cbaee4a9b0c0393
Freddy Febres <freddyrfa@gmail.com>
2024-01-13 11:11:05 -0400

    ✨ Better typing on getFilms hook

---
+++ utils/resources.ts
@@ -0,0 +1,17 @@
+export type Resources = {
+   films: string;
+   people: string;
+   planets: string;
+   species: string;
+   starships: string;
+   vehicles: string;
+};
+
+export const resources: Resources = {
+   films: 'films',
+   people: 'people',
+   planets: 'planets',
+   species: 'species',
+   starships: 'starships',
+   vehicles: 'vehicles'
+};
